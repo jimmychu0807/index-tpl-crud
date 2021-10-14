4bf9c87a5ca9eb799a2c21e123589e7278b7947e
Jimmy Chu <jimmychu0807@gmail.com>
2021-10-14 08:59:39 +0800

    beginning script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program.version('1.0.0');
+
+program
+  .command('init <ghRepo> <projectName> [outputPath]')
+  .description('initialize an index page')
+  .action((ghRepo, projectName, outputPath) => {
+    console.log(`init cmd called ${ghRepo}, ${projectName}, ${outputPath}`);
+  });
+
+program
+  .command('upsert <inputPath> <ref> [display]')
+  .description('upsert a REF into the index page')
+  .action((inputPath, ref, display) => {
+    console.log(`upsert cmd called ${inputPath}, ${ref}, ${display}`);
+  });
+
+program
+  .command('rm <inputPath> <ref>')
+  .description('remove a REF from the index page')
+  .action((inputPath, ref, display) => {
+    console.log(`rm cmd called ${inputPath}, ${ref}`);
+  });
+
+program.parse(process.argv);
